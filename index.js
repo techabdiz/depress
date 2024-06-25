ee9543ef6635e3f15f9af3f73d69f237d1eeb48c
Aziz Shaikh <tech.abdiz@gmail.com>
2024-06-26 01:15:21 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+app.get('/', (req, res)=> { 
+    name=req.query['name']?req.query['name']:'John Doe'
+    res.send({
+        message: 'hello, ' + name
+    })
+});
+
+app.listen(3000, ()=>{
+    console.log('app ready listening on port ' + port)
+})
