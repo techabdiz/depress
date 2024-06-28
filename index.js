d857dca87c2fa287cc5867e69c4b5d159c19f89f
Aziz Shaikh <tech.abdiz@gmail.com>
2024-06-28 10:27:39 +0530

    lines reduced

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 
 const port = 3000
-app.get('/', (req, res)=> { 
-    name=req.query['name']?req.query['name']:'John Doe'
+app.get('/', (req, res)=> {
     res.send({
-        message: 'Hello, ' + name
+        message: 'Hello, ' + req.query['name']||'John Doe'
     })
 });
 
